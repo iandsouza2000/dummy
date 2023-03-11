0ba51627aaf56fc4424d11f2c60766d2f12ab719
ian <ian@samespace.com>
2023-03-11 13:00:47 +0530

    Analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "animate.css/animate.min.css";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "G-FRJKDEVJ2V";
+ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
