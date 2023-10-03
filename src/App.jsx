6b6aa6b3a0d8042150f8d75556ca76174dbac921
ian <ian@samespace.com>
2023-10-03 23:40:31 +0530

    New Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import Demo from "./Demo";
 import FAQs from "./sections/FAQs";
 import Footer from "./sections/Footer";
 import Gallery from "./sections/Gallery";
@@ -8,24 +8,17 @@ import OurTeam from "./sections/OurTeam";
 import Roadmap from "./sections/Roadmap";
 import Vision from "./sections/Vision";
 
-import ReactGA from "react-ga";
-
-const TRACKING_ID = "G-FRJKDEVJ2V";
-ReactGA.initialize(TRACKING_ID);
-
 function App() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <Hero />
       <Gallery />
       <Roadmap />
       <OurTeam />
       <FAQs />
-      <Footer />
+      <Footer /> */}
+      <Demo />
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sehsaa/",
-  plugins: [react()],
+  base: "/",
+  plugins: [
+    react(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: "G-FRJKDEVJ2V",
+      },
+      enableDev: true,
+      config: {
+        cookie_domain: "auto",
+        cookie_expires: 63072000,
+        cookie_prefix: "sehsaa",
+        cookie_update: true,
+        cookie_flags: "",
+        send_page_view: true,
+        allow_google_signals: true,
+        allow_ad_personalization_signals: true,
+      },
+    }),
+  ],
 });
